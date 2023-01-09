865a264b28c2770aff389c2180bad56918f82685
SRYian <92420947+SRYian@users.noreply.github.com>
2023-01-09 20:54:39 +0700

    fix: Missing script on kencur prefab

---
+++ Assets/Scripts/ChangeView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeView : MonoBehaviour
+{
+    public GameObject lihatPeta;
+    public GameObject lihatKamera;
+    public Camera mapCamera;
+    public Camera debugPathCamera;
+
+    private bool _currentViewIsPeta;
+
+    public void OnButtonClick()
+    {
+        _currentViewIsPeta = !_currentViewIsPeta;
+        _changeMode();
+    }
+
+    private void _changeMode()
+    {
+        if (_currentViewIsPeta)
+        {
+            lihatPeta.SetActive(false);
+            lihatKamera.SetActive(true);
+            mapCamera.enabled = true;
+            debugPathCamera.enabled = true;
+        }
+        else
+        {
+            lihatPeta.SetActive(true);
+            lihatKamera.SetActive(false);
+            mapCamera.enabled = false;
+            debugPathCamera.enabled = false;
+        }
+    }
+}
